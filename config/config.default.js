06554c46787a9b326b815f132c3398d36225cd03
liyong <liyong@lanren-inc.com>
2022-02-15 19:15:07 +0800

    done 1

--- config/config.default.js
+++ config/config.default.js
@@ -1,11 +1,4 @@
 exports.eureka = {
-    default: {
-        ssl: true,
-        host: "user:password@localhost",
-        port: 443,
-        servicePath: "/eureka/apps",
-        registerWithEureka: false
-    },
     app: true,
     agent: false
 }
